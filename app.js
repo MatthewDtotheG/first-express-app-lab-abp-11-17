db852a4b8ea1d3ee697e2942b0633e991262952c
Matthew Garrett <mdgarrett1984@gmail.com>
2017-11-19 17:46:37 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 
 // Build Your Route Here
-app.get('/', function(req, resp) {});
+app.get('/', function(req, resp) {
+  console.log(req)
+  console.log(req)
+
+  resp.send("Hellow, World!")
+});
 
 module.exports = app;
+
+class Request{}
+
+class Response{}
